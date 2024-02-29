5f0796ea66de34a23a2ae48b41eca70e2e3e3b7d
Filip Gynthersen <filipg@live.dk>
2024-02-29 22:37:46 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/homepage.html');
+});
+
+app.get('/time/ip', async (req, res) => {
+    try {
+        const response = await axios.get('http://worldtimeapi.org/api/ip');
+
+        const { datetime, timezone } = response.data;
+
+        res.json({ datetime, timezone });
+    } catch (error) {
+        res.status(500).json({ error: 'Unable to fetch time' });
+    }
+});
+
+
+const PORT = 8080;
+app.listen(PORT, () => console.log("Server is running on port", PORT));
+
+module.exports = app;
